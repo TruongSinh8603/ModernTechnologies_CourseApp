c7303cb386e7e879ac590ca780165e25e684f344
TruongSinh8603 <truongsinh8603@gmail.com>
2024-01-07 04:38:59 +0700

    Buoi3

---
+++ CourseApp/courseapp/courses/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework import routers
+from courses import views
+
+router = routers.DefaultRouter()
+router.register('categories', views.CategoryViewSet, basename='categories')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
